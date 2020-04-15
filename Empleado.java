0405acc918c81d9c654fbee762bdea24ea8e15ce
Jair <jairsv05@gmail.com>
2020-04-14 21:06:48 -0500

    Intercambio inicial de proyecto

---
+++ Empleado.java
@@ -0,0 +1,10 @@
+
+public class Empleado extends Persona
+{
+
+    public Empleado()
+    {
+   
+    }
+
+}
---
+++ Persona.java
@@ -0,0 +1,21 @@
+
+
+public class Persona
+{
+   String nombre;
+   String apellido;
+   String genero;
+   String telefono;
+   
+    public Persona()
+    {
+    
+    }
+
+    public Persona(String nm,String ap,String gen, String tel){
+      this.nombre = nm;
+      this.apellido = ap;
+      this.genero = gen;
+      this.telefono = tel;
+    } 
+    }
